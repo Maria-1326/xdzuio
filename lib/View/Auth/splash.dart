260ac288ffe54493c24eb4f5308dbc49d284acf1
Maria Arshad <mariaarshad993@gmail.com>
2025-02-07 16:50:08 +0500

    first commit

--- lib/View/Auth/splash.dart
+++ lib/View/Auth/splash.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:xdzuio_app/core/utils/Themes/app_images.dart';
 
 class SplashScreen extends StatelessWidget {
-  const SplashScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
